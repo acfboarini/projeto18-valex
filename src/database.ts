16bc67a7c551b578aa302148a8759cc4a303abbb
Augusto <acfb.app@gmail.com>
2022-07-13 03:54:41 -0300

    preparando aplicacao p heroku

--- src/database.ts
+++ src/database.ts
@@ -2,7 +2,21 @@ import dotenv from "dotenv";
 import pg from "pg";
 dotenv.config();
 
-const { Pool } = pg;
+/*const { Pool } = pg;
 export const connection = new Pool({
   connectionString: process.env.DATABASE_URL,
-});
+});*/
+
+const { Pool } = pg;
+const configDatabase = {
+  connectionString: process.env.DATABASE_URL
+};
+
+if(process.env.MODE === "PROD") {
+  configDatabase.ssl = {
+    rejectUnauthorized: false
+  }
+}
+
+const connection = new Pool(configDatabase);
+export default connection;
